c9ca850281dc499f62ed247bc99688f50dff30d6
AlexStorm <info@alexstorm.ru>
2022-12-09 07:36:51 +0530

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import {NgModule} from '@angular/core';
+import {BootstrapComponent} from "@solenopsys/uimatrix-templates";
+import {IMPORTS_CONF, PROVIDERS_CONF, ROUTES} from "./conf";
+import {createNgxs} from "@solenopsys/lib-storage";
+import {environment} from "../environments/environment";
+import {RouterModule} from "@angular/router";
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forChild([...ROUTES]),
+    ...IMPORTS_CONF, ...createNgxs(!environment.production)
+  ],
+  providers: [...PROVIDERS_CONF],
+  bootstrap: [BootstrapComponent],
+})
+export class AppModule {
+}
+
+
---
+++ src/app/entry.module.ts
@@ -0,0 +1,16 @@
+import {NgModule} from "@angular/core";
+import {IMPORTS_CONF, PROVIDERS_CONF, ROUTES} from "./conf";
+import {createNgxs} from "@solenopsys/lib-storage";
+import {environment} from "../environments/environment";
+import {RouterModule} from "@angular/router";
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forChild([...ROUTES]),
+    ...IMPORTS_CONF,
+    ...createNgxs(!environment.production),
+  ],
+  providers: [...PROVIDERS_CONF],
+})
+export class RemoteEntryModule {}
